a8e98479351b3e8d56104a9c5c6640f9185170b8
Rafael Ferreira Mendes <sevendays20@hotmail.com>
2023-11-02 14:48:54 -0300

    commit cadastro e style

--- cadastro.php
+++ cadastro.php
@@ -8,49 +8,49 @@
 <title>Cadastro</title>
 </head>
 <body>
-    <div class="cadastro-mae">
-        <div class="card-form">
 
-        
+<div class="cadastro-mae">
+    <div class="card-form">
+        <img class="cadastro-logo" src="img/image 1.svg" alt="logo" width="100" height="100">
+        <h1>Cadastro de Usuário</h1>
+        <form class="cadastro-form" action="#" method="post">
+             <label class="cadastro_label" for="nome">Nome:</label>
+             <input class="cadastro-input" type="text" id="nome" name="nome" required>
+             <br>
+             <label class="cadastro_label" for="email">E-mail:</label>
+             <input class="cadastro-input" type="email" id="email" name="email" required>
+             <br>
+             <label class="cadastro_label" for="senha">Senha:</label>
+             <input class="cadastro-input" type="password" id="senha" name="senha" required>
+             <br>
+             <label class="cadastro_label3" for="confirmar-senha">Confirmar Senha:</label>
+             <input class="cadastro-input" type="password" id="confirmar-senha" name="confirmar-senha" required>
+             <br>
+             <div class="flex-row">
+                 <label class="cadastro_label1">Gênero:</label>
+                 <input class="cadastro-input3" type="radio" id="genero-masculino" name="genero" value="Masculino">
+                 <label class="cadastro_label1" for="genero-masculino">Masculino</label>
+                 <input class="cadastro-input3" type="radio" id="genero-feminino" name="genero" value="Feminino">
+                 <label class="cadastro_label1" for="genero-feminino">Feminino</label>
+                 <input class="cadastro-input3" type="radio" id="genero-outro" name="genero" value="Outro">
+                 <label class="cadastro_label1" for="genero-outro">Outro</label>
+                 </div>
+                 <br>
+            <label class="cadastro_label2" for="data-nascimento">Data de Nascimento:</label>
+            <input class="cadastro-input" type="date" id="data-nascimento" name="data-nascimento" required>
+            <br>
+            <label class="cadastro_label" for="cidade">Cidade:</label>
+            <input class="cadastro-input" type="text" id="cidade" name="cidade">
+            <br>
+            <label class="cadastro_label" for="pais">País:</label>
+            <input class="cadastro-input" type="text" id="pais" name="pais">
+            <br>
+            <div class="move-btn">
+                <button class="cadastro-btn" type="submit">Cadastrar</button>
+            </div>
 
-    
-    <img class="cadastro-logo" src="img/image 1.svg" alt="logo" width="100" height="100">
-<h1>Cadastro de Usuário</h1>
-<form class="cadastro-form" action="#" method="post">
-<label class="cadastro_label" for="nome">Nome:</label>
-<input class="cadastro-input" type="text" id="nome" name="nome" required>
-<br>
-<label class="cadastro_label" for="email">E-mail:</label>
-<input class="cadastro-input" type="email" id="email" name="email" required>
-<br>
-<label class="cadastro_label" for="senha">Senha:</label>
-<input class="cadastro-input" type="password" id="senha" name="senha" required>
-<br>
-<label class="cadastro_label3" for="confirmar-senha">Confirmar Senha:</label>
-<input class="cadastro-input" type="password" id="confirmar-senha" name="confirmar-senha" required>
-<br>
-<div class="flex-row">
-<label class="cadastro_label1">Gênero:</label>
-<input class="cadastro-input3" type="radio" id="genero-masculino" name="genero" value="Masculino">
-<label class="cadastro_label1" for="genero-masculino">Masculino</label>
-<input class="cadastro-input3" type="radio" id="genero-feminino" name="genero" value="Feminino">
-<label class="cadastro_label1" for="genero-feminino">Feminino</label>
-<input class="cadastro-input3" type="radio" id="genero-outro" name="genero" value="Outro">
-<label class="cadastro_label1" for="genero-outro">Outro</label>
-</div>
-<br>
-<label class="cadastro_label2" for="data-nascimento">Data de Nascimento:</label>
-<input class="cadastro-input" type="date" id="data-nascimento" name="data-nascimento" required>
-<br>
-<label class="cadastro_label" for="cidade">Cidade:</label>
-<input class="cadastro-input" type="text" id="cidade" name="cidade">
-<br>
-<label class="cadastro_label" for="pais">País:</label>
-<input class="cadastro-input" type="text" id="pais" name="pais">
-<br>
-<button class="cadastro-btn" type="submit">Cadastrar</button>
-</form>
-</div>
+        </form>
+    </div>
 </div>
 </body>
 </html>
