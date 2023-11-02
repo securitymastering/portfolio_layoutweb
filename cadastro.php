5dddbf07efb8b675474c21f42cac4fc1d38a6ae7
Rafael Ferreira Mendes <sevendays20@hotmail.com>
2023-11-02 14:17:48 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -29,13 +29,15 @@
 <label class="cadastro_label" for="confirmar-senha">Confirmar Senha:</label>
 <input class="cadastro-input" type="password" id="confirmar-senha" name="confirmar-senha" required>
 <br>
-<label>Gênero:</label>
+<div class="flex-row">
+<label class="cadastro_label">Gênero:</label>
 <input class="cadastro-input" type="radio" id="genero-masculino" name="genero" value="Masculino">
 <label class="cadastro_label1" for="genero-masculino">Masculino</label>
 <input class="cadastro-input" type="radio" id="genero-feminino" name="genero" value="Feminino">
 <label class="cadastro_label1" for="genero-feminino">Feminino</label>
 <input class="cadastro-input" type="radio" id="genero-outro" name="genero" value="Outro">
 <label class="cadastro_label1" for="genero-outro">Outro</label>
+</div>
 <br>
 <label class="cadastro_label" for="data-nascimento">Data de Nascimento:</label>
 <input class="cadastro-input" type="date" id="data-nascimento" name="data-nascimento" required>
